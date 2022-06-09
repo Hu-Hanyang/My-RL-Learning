58b8a5fe19e8a10eb30244c846e7938e28ee4050
Hanyang <HuHY97@outlook.com>
2022-06-09 15:30:08 +0800

    feat: add qpi(s,a) and vpi(s.)

--- 2-MDP/MDP_Students.py
+++ 2-MDP/MDP_Students.py
@@ -74,9 +74,81 @@ A = ['Play Phone', 'Study', 'Hold Phone', 'in Bar', 'Leave Study']  # 5 actions
 R = {}  # Rsa dictionary
 P = {}  # Pss'a dictionary
 gamma1 = 1.0
-
 # set transition probability based on the s, a and s'
 set_prob(P, S[0], A[0], S[0])
 set_prob(P, S[0], A[2], S[1])
+set_prob(P, S[1], A[0], S[0])
+set_prob(P, S[1], A[1], S[2])
+set_prob(P, S[2], A[1], S[3])
+set_prob(P, S[2], A[4], S[4])
+set_prob(P, S[3], A[1], S[4])
+set_prob(P, S[3], A[3], S[1], p=0.2)
+set_prob(P, S[3], A[3], S[2], p=0.4)
+set_prob(P, S[3], A[3], S[3], p=0.4)
+# set transition rewards based on the s and a
+set_reward(R, S[0], A[0], -1)
+set_reward(R, S[0], A[2], 0)
+set_reward(R, S[1], A[0], -1)
+set_reward(R, S[1], A[1], -2)
+set_reward(R, S[2], A[1], -2)
+set_reward(R, S[2], A[4], 0)
+set_reward(R, S[3], A[1], 10)
+set_reward(R, S[3], A[3], +1)
+MDP = (S, A, R, P, gamma)  # define MDP as a tuple
+# set random policy: pi(a|s) = 0.5
+Pi = {}
+set_pi(Pi, S[0], A[0], 0.5)
+set_pi(Pi, S[0], A[2], 0.5)
+set_pi(Pi, S[1], A[0], 0.5)
+set_pi(Pi, S[1], A[1], 0.5)
+set_pi(Pi, S[2], A[1], 0.5)
+set_pi(Pi, S[2], A[4], 0.5)
+set_pi(Pi, S[3], A[1], 0.5)
+set_pi(Pi, S[3], A[3], 0.5)
+# display_dict(Pi)
+# initialize value function
+V = {}
+# display_dict(V)
+
+
+# qpi(s,a) = r(s,a) + gamma \sum p vpi(s')
+def compute_q(MDP, V, s, a):
+    S, A, R, P, gamma = MDP
+    q_sa = 0
+    for s_prime in S:
+        q_sa += get_prob(P, s, a, s_prime) * get_value(V, s_prime)  # p vpi(s')
+        q_sa = get_reward(R, s, a) + gamma * q_sa
+        return q_sa
+
+
+# vpi(s) = \sum pi(a|s) * qpi(s,a)
+def compute_v(MDP, V, Pi, s):
+    S, A, R, P, gamma = MDP
+    v_s = 0
+    for a in A:
+        v_s += get_pi(Pi, s, a) * compute_q(MDP, V, s, a)
+    return v_s
+
+
+# use value iteration to calculate the value function v
+def policy_evaluation(MDP, V, Pi):
+    S, _, _, _, _ = MDP
+    V_prime = V.copy()
+    for s in S:
+        V_prime[str_key(s)] = compute_v(MDP, V_prime, Pi, s)
+    return V_prime
+
+
+def value_iteration(MDP, V, Pi, n):
+    for i in range(n):
+        V = policy_evaluation(MDP, V, Pi)
+    return V
+
+V = value_iteration(MDP, V, Pi, 100)
+display_dict(V)
+
+
+
+
 
 
