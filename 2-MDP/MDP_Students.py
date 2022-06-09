db6e840be6806f9695ad92978897708036641f52
Hanyang <HuHY97@outlook.com>
2022-06-09 11:20:37 +0800

    feat: add codes.

--- 2-MDP/MDP_Students.py
+++ 2-MDP/MDP_Students.py
@@ -62,7 +62,21 @@ def compute_value(Pss, rewards, num_states=7, gamma=0.05):
 values1 = compute_value(Pss, rewards, gamma=0.99999)
 # print(values1)
 
+# MDP
 # verify the q\pi(s,a)
 from utils import str_key, display_dict
 from utils import set_prob, set_reward, get_prob, get_reward
 from utils import set_value, set_pi, get_value, get_pi
+
+# construct MDP
+S = ['Phone', 'C1', 'C2', 'C3', 'Break']  # 5 states
+A = ['Play Phone', 'Study', 'Hold Phone', 'in Bar', 'Leave Study']  # 5 actions
+R = {}  # Rsa dictionary
+P = {}  # Pss'a dictionary
+gamma1 = 1.0
+
+# set transition probability based on the s, a and s'
+set_prob(P, S[0], A[0], S[0])
+set_prob(P, S[0], A[2], S[1])
+
+
