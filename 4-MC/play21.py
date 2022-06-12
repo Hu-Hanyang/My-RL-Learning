58c20a97c7ade7d6ad80c9cf6c73437bf785dabc
Hanyang <HuHY97@outlook.com>
2022-06-12 14:20:44 +0800

    "feat: add Moncte-Carlo"

---
+++ 4-MC/play21.py
@@ -0,0 +1,8 @@
+from random import shuffle
+from queue import Queue
+from tqdm import tqdm
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from utils import str_key, set_dict, get_dict
