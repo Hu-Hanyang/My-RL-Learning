93c99b0c1ca37923b89b77f0f52294f20ffcf1f7
Hanyang <HuHY97@outlook.com>
2022-06-12 19:13:41 +0800

    "feat: add some functions"

--- 4-MC/play21.py
+++ 4-MC/play21.py
@@ -6,3 +6,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 from utils import str_key, set_dict, get_dict
+
+
+# define a father class Game
+class Game():
+    def __init__(self, name='', A=None, display=False):
+        self.name = name
+        self.cards = []  # cards in hand
+        self.display = display  # whether display the details of the game
+        self.policy = None
+        self.learning_method = None
+        self.A = A  # action space
+
+    def __str__(self):
+        return self.name
+
+    def _value_of(self, card):
+        try:
+            v = int(card)
+        except:
+            if card == 'A':
+                v = 1
+            elif card in ['J', 'Q', 'K']:
+                v = 10
+            else:
+                v = 0
+        finally:
+            return v
+
+    def get_points(self):  # return total points and whether to use ace=1
+        num_of_usable_ace = 0
+        total_points = 0
+        cards = self.cards
+        if cards is None:
+            return 0, False
--- 4-MC/utils.py
+++ 4-MC/utils.py
@@ -1,4 +1,8 @@
-# 辅助函数
+import matplotlib.pyplot as plt
+import numpy as np
+import random  # 随机策略时用到
+
+
 def str_key(*args):
     '''将参数用"_"连接起来作为字典的键，需注意参数本身可能会是tuple或者list型，
     比如类似((a,b,c),d)的形式。
@@ -16,40 +20,67 @@ def set_dict(target_dict, value, *args):
     target_dict[str_key(*args)] = value
 
 
-def set_prob(P, s, a, s1, p=1.0):  # 设置概率字典
-    set_dict(P, p, s, a, s1)
+def get_dict(target_dict, *args):
+    return target_dict.get(str_key(*args), 0)
 
 
-def get_prob(P, s, a, s1):  # 获取概率值
-    return P.get(str_key(s, a, s1), 0)
+def greedy_pi(A, s, Q, a):
+    '''依据贪婪选择，计算在行为空间A中，状态s下，a行为被贪婪选中的几率
+    考虑多个行为的价值相同的情况
+    '''
+    # print("in greedy_pi: s={},a={}".format(s,a))
+    max_q, a_max_q = -float('inf'), []
+    for a_opt in A:  # 统计后续状态的最大价值以及到达到达该状态的行为（可能不止一个）
+        q = get_dict(Q, s, a_opt)
+        # print("get q from dict Q:{}".format(q))
+        if q > max_q:
+            max_q = q
+            a_max_q = [a_opt]
+        elif q == max_q:
+            # print("in greedy_pi: {} == {}".format(q,max_q))
+            a_max_q.append(a_opt)
+    n = len(a_max_q)
+    if n == 0: return 0.0
+    return 1.0 / n if a in a_max_q else 0.0
 
 
-def set_reward(R, s, a, r):  # 设置奖励字典
-    set_dict(R, r, s, a)
+def greedy_policy(A, s, Q):
+    """在给定一个状态下，从行为空间A中选择一个行为a，使得Q(s,a) = max(Q(s,))
+    考虑到多个行为价值相同的情况
+    """
+    max_q, a_max_q = -float('inf'), []
+    for a_opt in A:
+        q = get_dict(Q, s, a_opt)
+        if q > max_q:
+            max_q = q
+            a_max_q = [a_opt]
+        elif q == max_q:
+            a_max_q.append(a_opt)
+    return random.choice(a_max_q)
 
 
-def get_reward(R, s, a):  # 获取奖励值
-    return R.get(str_key(s, a), 0)
+def epsilon_greedy_pi(A, s, Q, a, epsilon=0.1):
+    m = len(A)
+    greedy_p = greedy_pi(A, s, Q, a)
+    # print("greedy prob:{}".format(greedy_p))
+    if greedy_p == 0:
+        return epsilon / m
+    n = int(1.0 / greedy_p)
+    return (1 - epsilon) * greedy_p + epsilon / m
 
 
-def display_dict(target_dict):  # 显示字典内容
-    for key in target_dict.keys():
-        print("{}:　{:.2f}".format(key, target_dict[key]))
-    print("")
-
-
-# 辅助方法
-def set_value(V, s, v):  # 设置价值字典
-    set_dict(V, v, s)
-
-
-def get_value(V, s):  # 获取价值值
-    return V.get(str_key(s), 0)
-
-
-def set_pi(Pi, s, a, p=0.5):  # 设置策略字典
-    set_dict(Pi, p, s, a)
-
-
-def get_pi(Pi, s, a):  # 获取策略（概率）值
-    return Pi.get(str_key(s, a), 0)
+def epsilon_greedy_policy(A, s, Q, epsilon, show_random_num=False):
+    pis = []
+    m = len(A)
+    for i in range(m):
+        pis.append(epsilon_greedy_pi(A, s, Q, A[i], epsilon))
+    rand_value = random.random()  # 产生一个0,1的随机数
+    # if show_random_num:
+    #    print("产生的随机数概率为:{:.2f}".format(rand_value))
+    # print(rand_value)
+    for i in range(m):
+        if show_random_num:
+            print("随机数:{:.2f}, 拟减去概率{}".format(rand_value, pis[i]))
+        rand_value -= pis[i]
+        if rand_value < 0:
+            return A[i]
