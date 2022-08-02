f76ee0b154da5247f1e226f2d07f377dcc80ccb7
Hanyang <HuHY97@outlook.com>
2022-08-02 11:20:43 +0800

    feat: add actor and critic network

--- 7-PG/network.py
+++ 7-PG/network.py
@@ -1,64 +1,100 @@
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
 import torch.nn.functional as F
-import copy
+
+EPS = 0.003
 
 
-class NetApproximator(nn.Module):  # Q 输入为s，输出为所有的（s,a）
-    def __init__(self, input_dim=1, output_dim=1, hidden_dim=32):
-        '''
-        Args:
-        :param input_dim: int
-        :param output_dim: int
-        :param hidden_dim: int
-        '''
-        super(NetApproximator, self).__init__()  # super 是啥意思？
-        self.linear1 = torch.nn.Linear(input_dim, hidden_dim)
-        self.linear2 = torch.nn.Linear(hidden_dim, output_dim)
+def fanin_init(size, fanin=None):
+    """
+    reference: https://arxiv.org/abs/1502.01852
+    """
+    fanin = fanin or size[0]
+    v = 1. / np.sqrt(fanin)
+    x = torch.Tensor(size).uniform_(-v, v)  # 从-v到v的均匀分布
+    return x.type(torch.FloatTensor)
 
-    def forward(self, x):
-        x = self._prepare_data(x)  # 数据预处理
-        h_relu = F.relu(self.linear1(x))  # 全神经网络之间的非线性处理
-        y_pred = self.linear2(h_relu)
-        return y_pred
 
-    def _prepare_data(self, x, requires_grad=False):
-        # convert numpy format data to torch variable
-        if isinstance(x, np.ndarray):  # isinstance 函数是做什么的?
-            x = torch.from_numpy(x)
-        if isinstance(x, int):
-            x = torch.Tensor([[x]])
-        x.requires_grad_ = requires_grad
-        x = x.float()  # 从from_numpy转换的数据为DoubleTensor类型
-        if x.data.dim == 1:
-            x = x.unsqueeze(0)  # unsqueeze函数功能是啥来着？
+class Critic(nn.Module):
+    def __int__(self, state_dim, action_dim):
+        """
+        Q(s,a) -> value
+        :param state_dim: int
+        :param action_dim: int
+        :return: torch.Tensor
+        """
+        super(Critic, self).__int__()
+        self.action_dim = action_dim
+        self.state_dim = state_dim
+        # state network architecture
+        self.fcs1 = nn.Linear(state_dim, 256)
+        self.fcs1.weight.data = fanin_init(self.fcs1.weight.data.size())
+        self.fcs2 = nn.Linear(256, 128)  # 状态第二次线性变换
+        self.fcs2.weight.data = fanin_init(self.fcs2.weight.data.size())
+        # action network architecture
+        self.fca1 = nn.Linear(action_dim, 128)  # 行为第一次线性变换
+        self.fca1.weight.data = fanin_init(self.fca1.weight.data.size())
+        # combination network architecture
+        self.fc2 = nn.Linear(256, 128)  # (状态+行为)联合的线性变换，注意参数值
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+        self.fc3 = nn.Linear(128, 1)  # (状态+行为)联合的线性变换
+        self.fc3.weight.data.uniform_(-EPS, EPS)
+
+    def forward(self, state, action):
+        """
+        forward propagation
+        :param state: torch.Tensor (n, state_dim)
+        :param action: torch.Tensor (n, action_dim)
+        :return: Q(s,a) torch.Tensor (n, 1)
+        """
+        # type conversion + forward propagation
+        state = torch.from_numpy(state)
+        state = state.type(torch.FloatTensor)
+        action = action.type(torch.FloatTensor)
+        s1 = F.relu(self.fcs1(state))
+        s2 = F.relu(self.fcs2(s1))
+        a1 = F.relu(self.fca1(action))
+        # concatenate
+        x = torch.cat((s2, a1), dim=1)
+        # final forward propagation
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
-    def fit(self, x, y, criterion=None, optimizer=None, epochs=1, learning_rate=1e-4):
-        # train + calculate error + back propagate
-        if criterion is None:
-            criterion = torch.nn.MSELoss(size_average=False)
-        if optimizer is None:
-            optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
-        if epochs < 1:
-            epochs = 1
-        y = self._prepare_data(y, requires_grad=False)  # y是选择的认为的真实值，即target
-        for t in range(epochs):
-            y_pred = self.forward(x)  # 前向传播
-            loss = criterion(y_pred, y)
-            optimizer.zero_grad()  # 梯度重置，准备接受新的梯度值
-            loss.backward()  # 反向传播，计算每个节点的梯度
-            optimizer.step()  # 更新权重
-        return loss
 
-    def __call__(self, x):  # 这个函数是怎么用的？
-        y_pred = self.forward(x)
-        return y_pred.data.numpy()
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, action_lim):
+        """
+        \pi(s) -> a
+        :param state_dim: int
+        :param action_dim: int
+        :param action_lim: [int, int]
+        """
+        super(Actor, self).__init__()
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.action_lim = action_lim
+        self.fc1 = nn.Linear(self.state_dim, 256)
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+        self.fc2 = nn.Linear(256, 128)
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+        self.fc3 = nn.Linear(128, 64)
+        self.fc3.weight.data = fanin_init(self.fc3.weight.data.size())
+        self.fc4 = nn.Linear(64, self.action_dim)
+        self.fc4.weight.data.uniform_(-EPS, EPS)
 
-    def clone(self):
-        # 获得当前模型的深度拷贝对象
-        return copy.deepcopy(self)
-
-
+    def forward(self, state):
+        """
+        forward propagation
+        :param state: torch.Tensor (n, state_dim)
+        :return: action torch.Tensor (n, action_dim)
+        """
+        state = torch.from_numpy(state)
+        state = state.type(torch.FloatTensor)
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        action = F.tanh(self.fc4(x))  # 输出范围-1,1
+        action = action * self.action_lim  # 更改输出范围
+        return action
